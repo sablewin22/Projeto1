db644a298688be9f942a8878309dcc48bfd3e741
sablewin22 <slewin2202@gmail.com>
2026-04-28 15:29:17 -0300

    Update teste2.py

--- teste2.py
+++ teste2.py
@@ -1,1 +1,8 @@
+import streamlit as st
 
+st.title('POKEMON TEMOS QUE PEGAR EU SEI :P')
+st.write('Abaixo estão alguns Pokemons!')
+st.image('https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/025.png')
+st.image('https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/001.png')
+st.image('https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/007.png')
+st.image('https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/004.png')
