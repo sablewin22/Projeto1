ec5793f5065176db65c78dbfd0c940620300649e
sablewin22 <slewin2202@gmail.com>
2026-04-28 15:17:30 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,8 +1,12 @@
 import streamlit as st
 
 st.title('Teste ECMI 1')
-st.write('Abaixo estão os pokemons mais famosos de todos!')
+st.write('Abaixo estão alguns Pokemons!')
 st.image('https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/025.png')
 st.image('https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/001.png')
 st.image('https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/007.png')
 st.image('https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/004.png')
+st.image('https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/037.png')
+st.image('https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/039.png')
+st.image('https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/060.png')
+st.image('https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/116.png')
