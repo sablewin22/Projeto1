7e2415b913a9f678e6a5a458c664874d24470113
sablewin22 <slewin2202@gmail.com>
2026-04-28 15:12:39 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,5 +2,7 @@ import streamlit as st
 
 st.title('Teste ECMI 1')
 st.write('Abaixo estão os pokemons mais famosos de todos!')
-st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwg2PWrY_5mkISXy_GqXWUYPbglvpL6FSUgg&s')
+st.image('https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/025.png')
 st.image('https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/001.png')
+st.image('https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/007.png')
+st.image('https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/004.png')
