cdb137cbf46e2825bd97ab11119520f6a2ce78fe
sablewin22 <slewin2202@gmail.com>
2026-04-28 15:46:00 -0300

    Delete teste2.py

--- teste2.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.title('O MAIOR FESTIVAL DE TODOS OS TEMPOS ESTÁ DE VOLTA: ROCK IN RIO 2026 VEM AI!')
-st.image('https://rapmidia.com.br/wp-content/uploads/2025/12/Rock-In-Rio-2026.jpg')
-st.write('O Rock in Rio 2026 promete ser histórico ao celebrar a diversidade musical no Parque Olímpico entre os dias 4 e 13 de setembro. O grande destaque desta edição é a estreia oficial do K-pop com o grupo Stray Kids, dividindo os holofotes com gigantes como Foo Fighters, Maroon 5 e Elton John. Além dos shows, o festival aposta em tecnologia com o espetáculo sensorial ECCO, consolidando-se mais uma vez como a maior experiência de entretenimento do país.
-Você já tem algum artista favorito ou dia específico que pretende garantir o ingresso?')
