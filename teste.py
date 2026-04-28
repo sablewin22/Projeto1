d3771fedad57e0c8950ea0fdd6670f5c07779cca
sablewin22 <slewin2202@gmail.com>
2026-04-28 15:23:40 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,12 +1,15 @@
 import streamlit as st
 
-st.title('Teste ECMI 1')
+st.title('POKEMON TEMOS QUE PEGAR EU SEI :P')
 st.write('Abaixo estão alguns Pokemons!')
 st.image('https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/025.png')
 st.image('https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/001.png')
 st.image('https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/007.png')
 st.image('https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/004.png')
-st.image('https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/037.png')
-st.image('https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/039.png')
-st.image('https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/060.png')
-st.image('https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/116.png')
+
+st.title('OS MELHHORES JOGADORES rs😈')
+st.write('Abaixo estão os jogadores mais bonitos!')
+st.image('https://admin.cnnbrasil.com.br/wp-content/uploads/sites/12/2022/10/GettyImages-1433916742-e1669266693306.jpg?w=1200&h=900&crop=0')
+st.image('https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcRxp_Sp9DBPtCFKIy5lCuSRgtoORsOeQNjkdSB6MHJUBAxWe1b9I1AOcCXg93LGN_6ggVjl8yq-hngQW2I')
+st.image('https://platform.barcablaugranes.com/wp-content/uploads/sites/21/2025/10/gettyimages-2243475324.jpg?quality=90&strip=all&crop=0.0048770971517698%2C0%2C99.990245805696%2C100&w=750')
+st.image('https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcRrZd5l5r75BuQYPSGeZGVi08iY-57JiwxNrn3vqMaeymw7P5pamaiew-NjIifbJIkOTWQbvDpDCmpdJeS1LNPrVjA5pdutFhHsaq82RYV5-0EycT7tTw8c2hQxcoQ8e1D2XMACwQwBNhVA&s=19')
