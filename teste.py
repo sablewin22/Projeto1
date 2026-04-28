d36a386f188a09ff6e0a33de12d720404c4800fc
sablewin22 <slewin2202@gmail.com>
2026-04-28 15:08:29 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -3,3 +3,4 @@ import streamlit as st
 st.title('Teste ECMI 1')
 st.write('Esse é o meu texto')
 st.write('oi :P')
+st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwg2PWrY_5mkISXy_GqXWUYPbglvpL6FSUgg&s')
