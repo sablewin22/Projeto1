1c8c2a828e0848291341d7a69156fc802f687a80
sablewin22 <slewin2202@gmail.com>
2026-04-28 15:11:15 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.title('Teste ECMI 1')
-st.write('Esse é o meu texto')
-st.write('oi :P')
+st.write('Abaixo estão os pokemons mais famosos de todos!')
 st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwg2PWrY_5mkISXy_GqXWUYPbglvpL6FSUgg&s')
+st.image('https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/001.png')
